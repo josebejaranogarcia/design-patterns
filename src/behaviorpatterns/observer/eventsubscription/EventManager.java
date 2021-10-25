ddadd60075e9f29c7e8b579e0d421bef9705223e
Jose Garcia <josefbgarcia3@gmail.com>
2021-10-25 18:54:22 +0200

    last refactoring observer

--- src/behaviorpatterns/observer/eventsubscription/EventManager.java
+++ src/behaviorpatterns/observer/eventsubscription/EventManager.java
@@ -10,27 +10,27 @@ import java.util.Map;
 
 public class EventManager {
 
-    Map<String, List<Subscriber>> eventsSubscribed = new HashMap<>();
+    Map<String, List<Subscriber>> subscribedEvents = new HashMap<>();
     List<Subscriber> subscribers;
 
     public EventManager(String... eventTypes) {
         for (String type : eventTypes)
-            this.eventsSubscribed.put (type, new ArrayList<>());
+            this.subscribedEvents.put (type, new ArrayList<>());
     }
 
 
     public void subscribe(String eventType, Subscriber subscriber) {
-        subscribers = eventsSubscribed.get(eventType);
+        subscribers = subscribedEvents.get(eventType);
         subscribers.add(subscriber);
     }
 
     public void unsubscribe(String eventType, Subscriber subscriber) {
-        subscribers = eventsSubscribed.get(eventType);
+        subscribers = subscribedEvents.get(eventType);
         subscribers.remove(subscriber);
     }
 
     public void notify(String eventType, File file) {
-        subscribers = eventsSubscribed.get(eventType);
+        subscribers = subscribedEvents.get(eventType);
         for (Subscriber listener : subscribers)
             listener.update( file);
     }
